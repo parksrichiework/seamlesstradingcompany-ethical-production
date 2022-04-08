4479c8fc6ed37a85cd14185ddb5b4b302651df52
Richie Parks <parksrichiework@gmail.com>
2022-04-08 14:02:19 -0500

    added html and css from existing files

---
+++ js/script.js
@@ -0,0 +1,13 @@
+     
+  var newVar = "things"
+
+  function multiplyThree(q,w,e){
+    return q * w * e
+  }
+
+  function turnOver(){
+    let product = multiplyThree(3,2,2)
+  }
+
+  console.log(turnOver())
+

1a4165988b8863e7f908aba303a1712ea29875df
Richie Parks <parksrichiework@gmail.com>
2022-04-25 13:20:32 -0500

    updated html and javascript
    made the following functions in js:
    - open sample request button
    - close sample request button
    - log information from form to console

--- js/main.js
+++ js/main.js
@@ -1,49 +1,8 @@
-     
-  
 
-//   function multiplyThree(q,w,e){
-//     return q * w * e
-//   }
+document.querySelectorAll('.standardsImg').addEventListener('click', open)
 
-// console.log(multiplyThree(3*2*2))
-
- 
-// h1Holder.addEventListener('click', sum);
-//     function sum(){
-//         console.log(num + Number(inputedVal))
-//     };
-
-
-// inputHolder.addEventListener('click', log)
-
-//     function log(){
-//         console.log()
-//     }
-
-
-// var formInputs= document.querySelector('.input')
-
-// console.log(formInputs)
-
-// modal for pop-up form
-
-
-//   function openForm() {
-//         document.getElementById("popupForm").style.display = "block";
-//       }
-//       function closeForm() {
-//         document.getElementById("popupForm").style.display = "none";
-//       }
-
-// var nameInput = document.getElementById('formContainer').value;
-
-// console.log(nameInput)
-// ;
-
-// function submitInfo(){
-//     document.querySelectorAll('submit-info')
-// }
-
-
-
-      //MODAL FOR POP UP FORM END
+function open(){
+      if (document.querySelector('#bci')){
+            function bci(){}
+      }
+}
